741557df769b86d1a67a7d9e714ff83c5ee59999
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-15 21:07:16 +0530

    complete the Layout function

---
+++ Frontend/src/Layout.jsx
@@ -0,0 +1,13 @@
+import MainFooter from "./Component/Landing/Footer";
+import Header from "./Component/Landing/Header";
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <h1>hi</h1>
+      <MainFooter />
+    </>
+  );
+}
+export default Layout;
