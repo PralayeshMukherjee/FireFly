b41765585400dccc2f52285cbfe8d6346aa55544
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-15 17:58:42 +0530

    insert the theme provider for the theme

---
+++ Frontend/src/Context/Theme.jsx
@@ -0,0 +1,22 @@
+import { Children, createContext, useEffect, useState } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => setThemeMode("light");
+  const darkTheme = () => setThemeMode("dark");
+
+  useEffect(() => {
+    document.documentElement.classList.remove("light", "dark");
+    document.documentElement.classList.add(themeMode);
+  }, [themeMode]);
+  return (
+    <ThemeContext.Provider value={{ themeMode, lightTheme, darkTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useTheme(ThemeContext);
