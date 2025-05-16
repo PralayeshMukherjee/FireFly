07721fc935ae3ddc84ffc38e1e821ee9db69064d
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-16 14:45:11 +0530

    add a new function for add a new user

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/UserController.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/UserController.java
@@ -2,13 +2,17 @@ package com.backend.FireFlyBackend.Controller;
 
 
 import com.backend.FireFlyBackend.DTO.AddUser;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    @Autowired
-    private AddUser addUser;
+
+    public Map<String,Boolean> addNewUser(@RequestBody AddUser addUser){
+
+    }
 }
