293826135f62aa79b4e2ae129db989620946b14e
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-16 22:27:27 +0530

    make the emailId as primary key by Id annotation

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/UserEntity.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/UserEntity.java
@@ -1,12 +1,15 @@
 package com.backend.FireFlyBackend;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "User")
 public class UserEntity {
     private String username;
+    @Id
     private String emailId;
     private String password;
+
 }
