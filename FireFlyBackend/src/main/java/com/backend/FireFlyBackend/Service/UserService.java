c6fa1b27665a7d6323423fdc401eca3b6df05e6e
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-16 15:05:16 +0530

    create a private static instance of otp length and a private map for storing the otp

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Service/UserService.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Service/UserService.java
@@ -2,6 +2,11 @@ package com.backend.FireFlyBackend.Service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class UserService {
+    private static final int otpLenght = 6;
+    private Map<String,String > otpMapping = new HashMap<>();
 }
