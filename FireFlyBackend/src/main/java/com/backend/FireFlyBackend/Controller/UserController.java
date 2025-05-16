c7e2b3d0359ea1fd9ff2d1ed410bde3052b75138
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-16 15:22:39 +0530

    add the instance of UserService method and autowired it

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/UserController.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/UserController.java
@@ -2,6 +2,8 @@ package com.backend.FireFlyBackend.Controller;
 
 
 import com.backend.FireFlyBackend.DTO.AddUser;
+import com.backend.FireFlyBackend.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +14,13 @@ import java.util.Map;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
     @PostMapping("/register")
     public Map<String,Boolean> addNewUser(@RequestBody AddUser addUser){
         String name = addUser.getName().trim();
         String emailId = addUser.getEmailId().trim();
         String password = addUser.getPassword().trim();
+
     }
 }
