16dd3fcfdd90ba0c288b09a9d2fd5f74c6769a6d
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-16 14:40:33 +0530

    add the instance of AddUser and autowired it

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/UserController.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/UserController.java
@@ -1,11 +1,14 @@
 package com.backend.FireFlyBackend.Controller;
 
 
+import com.backend.FireFlyBackend.DTO.AddUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
+    @Autowired
+    private AddUser addUser;
 }
