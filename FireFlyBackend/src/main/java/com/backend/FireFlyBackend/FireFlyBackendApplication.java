fff2676773adb057054633ef8aa32c8ae76bb52f
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-16 13:33:11 +0530

    init the backend

---
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/FireFlyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.FireFlyBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireFlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireFlyBackendApplication.class, args);
+	}
+
+}
---
+++ FireFlyBackend/src/test/java/com/backend/FireFlyBackend/FireFlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.FireFlyBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireFlyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
