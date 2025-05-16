1a5d2020745d29987893d19d40a610a51750d860
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-16 14:39:09 +0530

    add the getter and setter method for the private data members

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/DTO/AddUser.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/DTO/AddUser.java
@@ -13,4 +13,28 @@ public class AddUser {
         this.emailId = emailId;
         this.password = password;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
