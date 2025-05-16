b6a89970da1da0c3b536610fa1460ffe72d06ab1
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-16 22:38:54 +0530

    extend the jpa repository

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/repository/UserRepository.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.backend.FireFlyBackend.repository;
 
+import com.backend.FireFlyBackend.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<UserEntity,String> {
+
 }
