471cf8539322d23630fb63e32990ff63275da93d
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-16 14:45:44 +0530

    add the postmapping

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/UserController.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/UserController.java
@@ -2,6 +2,7 @@ package com.backend.FireFlyBackend.Controller;
 
 
 import com.backend.FireFlyBackend.DTO.AddUser;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
+    @PostMapping("/register")
     public Map<String,Boolean> addNewUser(@RequestBody AddUser addUser){
 
     }
