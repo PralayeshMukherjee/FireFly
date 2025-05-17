78592aa33cea640b1e7f5ca72d9f09357fb22ecf
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-17 20:10:04 +0530

    add a method that call the api

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/APIController.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/APIController.java
@@ -1,4 +1,12 @@
 package com.backend.FireFlyBackend.Controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
 public class APIController {
+    public Mono<Map<String,Object>> fireflyChat(@RequestBody String userMessage){
+
+    }
 }
