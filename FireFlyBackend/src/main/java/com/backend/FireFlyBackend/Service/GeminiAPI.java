5c296321e0bc8c8cb0c3b2c1571c54001aa28bea
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-17 20:42:47 +0530

    complete the function of api calling in details

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Service/GeminiAPI.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Service/GeminiAPI.java
@@ -1,10 +1,55 @@
 package com.backend.FireFlyBackend.Service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @Service
 public class GeminiAPI {
     @Value("${gemini.api.key}")
     private String apiKey;
+
+    private final WebClient webClient;
+
+    public GeminiAPI(WebClient.Builder webClientBuilder){
+        this.webClient = webClientBuilder.baseUrl("https://generativelanguage.googleapis.com/v1beta/").build();
+    }
+
+    public Mono<Map<String, Object>> chat(String userMessage) {
+        String formattedPrompt = "";
+        userMessage = userMessage.trim().toLowerCase();
+        if(userMessage.contains("medicine")){
+            formattedPrompt = "suggest medicine and give me response in 50-60 words: "+ userMessage;
+        }else if(userMessage.equalsIgnoreCase("hey") || userMessage.equalsIgnoreCase("hi")){
+            return Mono.just(Map.of(
+                    "response", "This is your firefly chatbot. How can I help you?"
+            ));
+        }else if(userMessage.equalsIgnoreCase("thanks") || userMessage.equalsIgnoreCase("thank you")){
+            return Mono.just(Map.of(
+                    "response", "Mention not. I am here to help you always, good day"
+            ));
+        }else if(userMessage.contains("symptoms")){
+            formattedPrompt = "what is the symptoms of and give me response in 50-60 words: "+userMessage;
+        }else if(userMessage.contains("disease")){
+            formattedPrompt = "explain and give me response in 50-60 words: "+ userMessage;
+        }else{
+            formattedPrompt = "give me response in 50-60 words on the: "+userMessage;
+        }
+        return webClient.post()
+                .uri(uriBuilder -> uriBuilder.path("/models/gemini-2.0-flash:generateContent").queryParam("key", apiKey).build())
+                .bodyValue(Map.of(
+                        "contents", new Object[]{
+                                Map.of("parts", new Object[]{
+                                        Map.of("text", formattedPrompt)
+                                })
+                        }
+                ))
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {})
+                .map(response -> response);
+    }
 }
