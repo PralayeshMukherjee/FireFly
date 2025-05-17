f7c51e0a499a318f8c63f120e10de42925948ecb
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-17 20:44:14 +0530

    add the private instance of gemini api service calss and autowired it

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/APIController.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/APIController.java
@@ -1,5 +1,7 @@
 package com.backend.FireFlyBackend.Controller;
 
+import com.backend.FireFlyBackend.Service.GeminiAPI;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +12,11 @@ import java.util.Map;
 @RestController
 @RequestMapping("api")
 public class APIController {
+
+    @Autowired
+    private GeminiAPI geminiAPI;
+
     @PostMapping("chat")
     public Mono<Map<String,Object>> fireflyChat(@RequestBody String userMessage){
-
     }
 }
