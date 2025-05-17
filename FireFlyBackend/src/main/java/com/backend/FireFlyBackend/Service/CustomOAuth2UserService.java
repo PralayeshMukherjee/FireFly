0625d8aeda53b6675c56c2c7219922251f73e7ca
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-17 23:45:27 +0530

    add the instance of User Repo

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Service/CustomOAuth2UserService.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Service/CustomOAuth2UserService.java
@@ -1,4 +1,14 @@
 package com.backend.FireFlyBackend.Service;
 
-public class CustomOAuth2UserService {
+import com.backend.FireFlyBackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    @Autowired
+    private UserRepository userRepository;
+
 }
