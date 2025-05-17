526644fcb999cd91b37e0e631518f715c1f19b0e
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-17 20:21:49 +0530

    add the private instance of api key

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Service/GeminiAPI.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Service/GeminiAPI.java
@@ -1,7 +1,10 @@
 package com.backend.FireFlyBackend.Service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GeminiAPI {
+    @Value("${gemini.api.key}")
+    private String apiKey;
 }
