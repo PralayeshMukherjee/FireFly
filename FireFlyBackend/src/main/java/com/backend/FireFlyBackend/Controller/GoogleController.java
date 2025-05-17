325286cdcb9ba6dede4af565c3c692ab01549d39
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-17 22:41:49 +0530

    complete the function

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/GoogleController.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/GoogleController.java
@@ -1,7 +1,14 @@
 package com.backend.FireFlyBackend.Controller;
 
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class GoogleController {
+    public String googleLogin(OAuth2AuthenticationToken authentication){
+        Map<String,Object> map = authentication.getPrincipal().getAttributes();
+        return "Hello, "+map.get("name");
+    }
 }
