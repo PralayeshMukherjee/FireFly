c794351f10b19fd9fcac75d587bd0aa42fc893ea
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-18 00:55:39 +0530

    add the method that return the user is authenticate or not

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/GoogleController.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/GoogleController.java
@@ -1,16 +1,34 @@
 package com.backend.FireFlyBackend.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/check")
 public class GoogleController {
-    @GetMapping("/home")
-    public String googleLogin(OAuth2AuthenticationToken authentication){
-        Map<String,Object> map = authentication.getPrincipal().getAttributes();
-        return "Hello, "+map.get("name");
+    @GetMapping("/login")
+    public ResponseEntity<?> getUser(OAuth2AuthenticationToken auth) {
+        if (auth == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Not logged in");
+        }
+
+        Map<String, Object> attributes = auth.getPrincipal().getAttributes();
+
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("email", attributes.get("email"));
+        userData.put("name", attributes.get("name"));
+        userData.put("picture", attributes.get("picture"));
+        userData.put("isGoogleUser", true);
+
+        return ResponseEntity.ok(userData);
     }
+
 }
