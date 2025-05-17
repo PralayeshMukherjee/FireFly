071a209c20660e3d2b78654ee5d2f2d7a6ed5eff
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-17 20:11:17 +0530

    add the request mappings

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/APIController.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/APIController.java
@@ -1,11 +1,16 @@
 package com.backend.FireFlyBackend.Controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.util.Map;
-
+@RestController
+@RequestMapping("api")
 public class APIController {
+    @PostMapping("chat")
     public Mono<Map<String,Object>> fireflyChat(@RequestBody String userMessage){
 
     }
