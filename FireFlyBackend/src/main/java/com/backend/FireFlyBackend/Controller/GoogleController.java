27c6b77d42677178d8041ca0151a47439d5c6d73
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-17 22:50:23 +0530

    add the get mapping

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/GoogleController.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/GoogleController.java
@@ -1,12 +1,14 @@
 package com.backend.FireFlyBackend.Controller;
 
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
 public class GoogleController {
+    @GetMapping("/home")
     public String googleLogin(OAuth2AuthenticationToken authentication){
         Map<String,Object> map = authentication.getPrincipal().getAttributes();
         return "Hello, "+map.get("name");
