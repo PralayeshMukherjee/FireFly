012ea6f094a021aa9f600d23493614720fe366ab
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-19 01:05:15 +0530

    modify the gemini api prompt

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Service/GeminiAPI.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Service/GeminiAPI.java
@@ -23,7 +23,7 @@ public class GeminiAPI {
         String formattedPrompt = "";
         userMessage = userMessage.trim().toLowerCase();
         if(userMessage.contains("medicine")){
-            formattedPrompt = "suggest medicine and give me response in 50-60 words: "+ userMessage;
+            formattedPrompt = "suggest medicine and give me response in 100-150 words and attached bullet points about the medicines : "+ userMessage+" and don't use 'I am an AI assistant, and cannot provide medical advice.' this line or other any warnings "+" and add a summary of all the things that you mention";
         }else if(userMessage.equalsIgnoreCase("hey") || userMessage.equalsIgnoreCase("hi")){
             return Mono.just(Map.of(
                     "response", "This is your firefly chatbot. How can I help you?"
@@ -33,11 +33,11 @@ public class GeminiAPI {
                     "response", "Mention not. I am here to help you always, good day"
             ));
         }else if(userMessage.contains("symptoms")){
-            formattedPrompt = "what is the symptoms of and give me response in 50-60 words: "+userMessage;
+            formattedPrompt = "what is the symptoms of and give me response in 50-60 words paragraph and some bullet points related to medicine or help care related topic on the: "+userMessage+" and don't use 'I am an AI assistant, and cannot provide medical advice.' this line or other any warnings";
         }else if(userMessage.contains("disease")){
-            formattedPrompt = "explain and give me response in 50-60 words: "+ userMessage;
+            formattedPrompt = "explain and give me response in 50-60 words paragraph and some bullet points related to medicine or help care related topic on the: "+ userMessage+" and don't use 'I am an AI assistant, and cannot provide medical advice.' this line or other any warnings";
         }else{
-            formattedPrompt = "give me response in 50-60 words related to medicine or help care related topic on the: "+userMessage;
+            formattedPrompt = "give me response in 50-60 words paragraph and some bullet points related to medicine or help care related topic on the: "+userMessage+" and don't use 'I am an AI assistant, and cannot provide medical advice.' this line or other any warnings";
         }
         return webClient.post()
                 .uri(uriBuilder -> uriBuilder.path("/models/gemini-2.0-flash:generateContent").queryParam("key", apiKey).build())
