e725f76d31bb8d6546e4cb11fe6c7f5724bd6998
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-21 00:38:02 +0530

    add the instance of the two repository

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Service/ChatService.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Service/ChatService.java
@@ -1,7 +1,11 @@
 package com.backend.FireFlyBackend.Service;
 
+import com.backend.FireFlyBackend.repository.UserDataRepo;
+import com.backend.FireFlyBackend.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatService {
+    private final UserRepository userRepository;
+    private final UserDataRepo userDataRepo;
 }
