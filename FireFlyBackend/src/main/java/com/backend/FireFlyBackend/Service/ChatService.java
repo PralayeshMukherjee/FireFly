d2696f0e6e8cdb90e30e53c4595bcedd1c1aa0c7
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-21 00:43:22 +0530

    complete the functions

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Service/ChatService.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Service/ChatService.java
@@ -1,8 +1,12 @@
 package com.backend.FireFlyBackend.Service;
 
+import com.backend.FireFlyBackend.entity.UserData;
+import com.backend.FireFlyBackend.entity.UserEntity;
 import com.backend.FireFlyBackend.repository.UserDataRepo;
 import com.backend.FireFlyBackend.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ChatService {
@@ -12,5 +16,16 @@ public class ChatService {
     public ChatService(UserRepository userRepository,UserDataRepo userDataRepo){
         this.userRepository = userRepository;
         this.userDataRepo = userDataRepo;
+    }
+    public void saveUserMessage(String email, String message){
+        Optional<UserEntity> uE = userRepository.findById(email);
+        if(uE.isPresent()){
+            UserData userData = new UserData();
+            userData.setMessage(message);
+            userData.setEmail(email);
+            userDataRepo.save(userData);
+        }else {
+            throw new RuntimeException("User not found with email: " + email);
+        }
     }
 }
