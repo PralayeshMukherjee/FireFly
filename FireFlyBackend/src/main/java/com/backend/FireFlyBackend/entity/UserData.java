aead1aed3fba22b1a924c74d3fb592c79223782b
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-05-21 00:23:35 +0530

    added the constructor

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/entity/UserData.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/entity/UserData.java
@@ -12,4 +12,13 @@ public class UserData {
     @ManyToOne
     @JoinColumn(name = "email_id", referencedColumnName = "email_id")
     private String email;
+
+    public UserData() {
+    }
+
+    public UserData(Long id, String message, String email) {
+        this.id = id;
+        this.message = message;
+        this.email = email;
+    }
 }
