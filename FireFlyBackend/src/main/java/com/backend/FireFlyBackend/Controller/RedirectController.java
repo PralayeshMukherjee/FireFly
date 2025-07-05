53ece6b1d498ef508b13a78f6397ab4f2ee83a9c
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-06 02:46:16 +0530

    complete a function that redirect to /chatbot

--- FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/RedirectController.java
+++ FireFlyBackend/src/main/java/com/backend/FireFlyBackend/Controller/RedirectController.java
@@ -1,4 +1,16 @@
 package com.backend.FireFlyBackend.Controller;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
 public class RedirectController {
+    @GetMapping("/redirect")
+    public void redirectAfterLogin(HttpServletResponse response) throws IOException {
+        response.sendRedirect("https://fireflyfrontend-7y99.onrender.com/chatbot");
+    }
+
 }
