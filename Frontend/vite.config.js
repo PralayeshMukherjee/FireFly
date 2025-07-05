c35831fa65cd8dba9b854206101b02daa4f71beb
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-06 03:08:10 +0530

    modify the vite configuration

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+import fs from 'fs'
+
+const copyRedirects = () => {
+  return {
+    name: 'copy-redirects',
+    buildEnd() {
+      const src = resolve(__dirname, 'public/_redirects')
+      const dest = resolve(__dirname, 'dist/_redirects')
+      fs.copyFileSync(src, dest)
+    }
+  }
+}
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss(),copyRedirects()],
 })
