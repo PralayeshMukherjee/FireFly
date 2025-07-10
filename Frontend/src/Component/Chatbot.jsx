21cf95177995a1c14b0b5ff96519bdd95b6c82d0
SjxSubham <sjxsubham@gmail.com>
2025-07-10 22:32:34 +0530

    gradient

--- Frontend/src/Component/Chatbot.jsx
+++ Frontend/src/Component/Chatbot.jsx
@@ -618,7 +618,7 @@ const Chatbot = () => {
             className="mr-2 w-25 h-20 rounded-full"
           />
           </Link>
-          <span className="text-2xl dark:bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block bg-clip-text gap-2  justify-between  font-bold">FireFly Chatbot</span>
+          <span className="text-2xl  gap-2  justify-between  font-bold">FireFly Chatbot</span>
         </div>
 
         <div className="flex gap-4 space-x-4 mx-4">
