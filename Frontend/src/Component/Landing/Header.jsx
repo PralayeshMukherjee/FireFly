304081577df50c15099b16cbc63ec5cdb38ce4ea
SjxSubham <sjxsubham@gmail.com>
2025-07-10 22:08:20 +0530

    push

--- Frontend/src/Component/Landing/Header.jsx
+++ Frontend/src/Component/Landing/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
     <header className="shadow sticky z-50 top-0">
       <nav className="bg-violet-600  dark:bg-gray-800 border-gray-200 px-4 lg:px-6 py-2">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <Link to="/" className="flex bg-blue-200 dark:bg-transparent rounded-xl items-center">
+          <Link to="/" className="flex dark:bg-transparent rounded-xl items-center">
             <img
               src={fireflyLogo}
               className="mr-2 w-25 h-20 rounded-full"
